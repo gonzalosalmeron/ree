ed28866a0faec967292b6eead2407adcfe08b065
Gonzalo Salmeron <gonzxlosalmeron@gmail.com>
2024-12-15 20:56:55 +0100

    fix: Show first initial on user profile

--- src/components/dashboard/navbar-user-profile.tsx
+++ src/components/dashboard/navbar-user-profile.tsx
@@ -29,14 +29,16 @@ export default async function NavbarUserProfile() {
         <div id='btn-show-menu-options-r0'>
           <div className='flex items-center gap-2'>
             <div className='mt-0.5 flex aspect-square h-9 w-9 shrink-0 items-center justify-center rounded-full bg-accent'>
-              <p className='text-lg uppercase text-accent-foreground'>g</p>
+              <p className='text-lg uppercase text-accent-foreground'>
+                {session?.user?.name?.charAt(0) ?? '-'}
+              </p>
             </div>
             <div className='hidden overflow-hidden sm:block'>
               <p className='truncate text-[14.5px] text-primary'>
-                {session?.user?.name}
+                {session?.user?.name ?? '------'}
               </p>
               <p className='truncate text-xs text-secondary-foreground/60'>
-                {session?.user?.email}
+                {session?.user?.email ?? '------'}
               </p>
             </div>
           </div>
